c9b8ed077b70c1e27fa961de1bf55ebcd2a638d5
wlgh325 <wlgh325@naver.com>
2026-03-01 18:34:51 +0900

    Solve maxCounters

---
+++ lessons/CountingElements/MaxCounters/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] solution(int N, int[] A) {
+        int[] result = new int[N];
+        int max = 0;
+        int base = 0;
+
+        for (int i=0; i<A.length; i++) {
+            int idx = A[i] - 1;
+            if (A[i] == (N+1)) {
+                base = max;
+            } else {
+                if (base > result[idx]) {
+                    result[idx] = base;
+                    result[idx]++;
+                } else {
+                    result[idx]++;
+                }
+
+                // Set max
+                if (result[idx] > max) {
+                    max = result[idx];
+                }
+            }
+        }
+
+        // lazy update
+        for (int i=0; i<result.length; i++) {
+            if (result[i] < base) {
+                result[i] = base;
+            }
+        }
+        return result;
+    }
+}
